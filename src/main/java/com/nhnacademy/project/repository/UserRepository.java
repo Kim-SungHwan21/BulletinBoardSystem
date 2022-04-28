0478df25f26710facb90c1c881e411b6a70f4503
sunghwanKim <shkcreate@naver.com>
2022-04-28 14:09:17 +0900

    Maven setting, JSP setting and Repository..

---
+++ src/main/java/com/nhnacademy/project/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.project.repository;
+
+import java.util.List;
+
+public interface UserRepository {
+    void add(User user);
+    void modify(User user);
+    User remove(String id);
+
+    User getUser(String id);
+    List<User> getUsers();
+
+}
